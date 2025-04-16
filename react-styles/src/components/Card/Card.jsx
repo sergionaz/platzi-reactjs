5b5f3fa599f7ba4087745756d4ad763b39cac989
Sergio Nazar <nsergio.nazar@gmail.com>
2025-04-16 20:25:51 -0300

    Styles en linea

--- react-styles/src/components/Card/Card.jsx
+++ react-styles/src/components/Card/Card.jsx
@@ -3,8 +3,16 @@
 import styles from "./Card.module.sass"
 
 const Card = () => {
+    const textStyles = {
+        color: "blue", 
+        fontSize: "2rem",
+        textAlign: "center"
+    }
+
     return (
-        <div className={styles.card}>
+        // <div className={styles.card}>
+        // <div style={{ color: "blue", fontSize: "2rem"}}>
+        <div style={textStyles}>
             Soy una Card!
         </div>
     )
