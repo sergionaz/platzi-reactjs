c3d0f7fcfb2f6af0a6e33714bd60111e1855cc32
Sergio Nazar <nsergio.nazar@gmail.com>
2025-05-28 21:27:23 -0300

    Estados complejos y globales

--- react-data/src/App.jsx
+++ react-data/src/App.jsx
@@ -1,12 +1,54 @@
 // import StaticComponent from './components/StaticComponent/StaticComponent' 
 // import UserList from './components/UserList/UserList'
-import SearchPosts from './components/SearchPosts/SearchPosts'
+// import SearchPosts from './components/SearchPosts/SearchPosts'
+import { useContext, createContext, useState } from "react"
+import Counter from './components/Counter/Counter'
 import './App.css'
+import React from 'react'
+
+const ThemeContext = createContext();
+
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+  }
+
+  return (
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+
+}
+
+function ThemeButton() {
+  const { theme, toggleTheme } = useContext(ThemeContext)
+
+  return(
+    <button
+      onClick={toggleTheme}
+      style={{
+        backgroundColor: theme === 'light' ? '#FFF' : '#333',
+        color: theme === 'light' ? '#000' : '#FFF',
+      }}
+    >
+      Cambiar tema
+    </button>
+  )
+}
 
 function App() {
   // return <StaticComponent/>
   // return <UserList/>
-  return <SearchPosts/>
+  // return <SearchPosts/>
+  // return <Counter/>
+  return (
+    <ThemeProvider>
+      <ThemeButton/>
+    </ThemeProvider>
+  )
 }
 
 export default App
